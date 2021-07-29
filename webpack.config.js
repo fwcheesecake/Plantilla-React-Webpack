c032d906789181a74c7f44e61e92c047bd07d6d8
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-07-29 00:58:20 -0500

    Se agregara una configuracion de webpack donde nos deje manejar rutas

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,9 @@ module.exports = {
       
         ]
     },
+     devServer: {
+        historyApiFallback: true,
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: './public/index.html',
