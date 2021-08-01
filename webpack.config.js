a8809ae9da6304a5a783f105cfd66991cafce35b
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-08-01 04:56:46 -0500

    Se agregara la carpeta de inicio por si manejamos otras rutas

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,86 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const postcssOptions = require('postcss-loader');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postcssOptions = require("postcss-loader");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use:{
-                    loader: 'html-loader',
-                }
-            },
-            {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+      {
         test: /\.(scss)$/,
-        use: [{
-          // inject CSS to page
-          loader: 'style-loader'
-        }, {
-          // translates CSS into CommonJS modules
-          loader: 'css-loader'
-        }, {
-          // Run postcss actions
-          loader: 'postcss-loader',
-          options: {
-            // `postcssOptions` is needed for postcss 8.x;
-            // if you use postcss 7.x skip the key
-            postcssOptions: {
-              // postcss plugins, can be exported to postcss.config.js
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }
-        }, {
-          // compiles Sass to CSS
-          loader: 'sass-loader'
-        }]
+        use: [
+          {
+            // inject CSS to page
+            loader: "style-loader",
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: "css-loader",
+          },
+          {
+            // Run postcss actions
+            loader: "postcss-loader",
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require("autoprefixer")];
+                },
+              },
             },
-            {
-              test: /\.(png|gif|jpg)$/,
-              use: [
-                  {
-                      'loader': 'file-loader',
-                      options: {
-                          name: 'assets/[hash].[ext]'
-                      }
-                  }
-              ]
-          }
-      
-        ]
-    },
-     devServer: {
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].css',
-          }),
-    ]
-}
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "assets/[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+  ],
+};
